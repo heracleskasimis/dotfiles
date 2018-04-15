96dcad6f3f3c2f88b313eaf129dede8f3ed424be
Heracles Kasimis <heracleskasimis@gmail.com>
2018-04-15 10:31:23 +0300

    removes unused faces

--- .spacemacs.d/themes/cyan-light-theme.el
+++ .spacemacs.d/themes/cyan-light-theme.el
@@ -402,11 +402,9 @@ to 'auto, tags may not be properly aligned. "
      `(git-gutter-fr+-added ((,class (:foreground ,green :inherit bold))))
      `(git-gutter-fr+-deleted ((,class (:foreground ,red :inherit bold))))
      `(git-gutter-fr+-modified ((,class (:foreground ,war :inherit bold))))
-     `(git-gutter-fr+-unchanged ((,class (:background ,yellow-bg-s))))
      `(git-gutter-fr:added ((,class (:foreground ,green :inherit bold))))
      `(git-gutter-fr:deleted ((,class (:foreground ,red :inherit bold))))
      `(git-gutter-fr:modified ((,class (:foreground ,war :inherit bold))))
-     `(git-gutter-fr:unchanged ((,class (:background ,yellow-bg-s))))
 
 ;;;;; git-timemachine
      `(git-timemachine-minibuffer-detail-face ((,class (:foreground ,blue :inherit bold :background ,blue-bg))))
