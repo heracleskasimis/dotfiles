0f2e94e1070e3ab26e0294574e36da3f5c634f39
Heracles Kasimis <heracleskasimis@gmail.com>
2018-05-16 20:21:16 +0300

    fixes javascript-extras config

--- .doom.d/modules/lang/javascript-extras/config.el
+++ .doom.d/modules/lang/javascript-extras/config.el
@@ -1,4 +1,6 @@
 ;;; lang/javascript-extras/config.el -*- lexical-binding: t; -*-
 
+(require 'prettier-js)
+
 (add-hook 'js2-mode-hook 'prettier-js-mode)
 (add-hook 'web-mode-hook 'prettier-js-mode)
